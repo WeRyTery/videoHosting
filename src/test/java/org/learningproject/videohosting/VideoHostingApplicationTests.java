76e7d822906957dbc7c4946d99d2604cd81ff635
Ghost <echanakly@gmail.com>
2025-03-23 17:23:07 +0200

    init commit

---
+++ src/test/java/org/learningproject/videohosting/VideoHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learningproject.videohosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoHostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
