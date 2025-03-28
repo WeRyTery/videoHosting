ce9cb7c0482ab5cc516f6f2a4dc5dd6d8ff9c4a3
Ghost <echanakly@gmail.com>
2025-03-28 10:04:25 +0200

    Basic level logic added Basic controllers, service, model. Connected PostgreSQL DB You can now upload, view and download video files.

--- src/main/java/org/learningproject/videohosting/controller/DownloadVideoController.java
+++ src/main/java/org/learningproject/videohosting/controller/DownloadVideoController.java
@@ -1,34 +1,34 @@
-package org.learningproject.videohosting.controller;
-
-import org.learningproject.videohosting.model.Video;
-import org.learningproject.videohosting.service.VideoService;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-@RestController
-public class DownloadVideoController {
-    private final VideoService videoService;
-
-    public DownloadVideoController(VideoService videoService) {
-        this.videoService = videoService;
-    }
-
-    @GetMapping("/video/watch/{id}")
-    public ResponseEntity<byte[]> watchVideoById(@PathVariable Integer id){
-        Video video = videoService.getVideo(id);
-        if(video == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType(video.getContentType()));
-        headers.set("Content-Disposition", "inline; filename=\"" + video.getFileName() + "\"");
-        byte[] data = video.getData();
-
-        return new ResponseEntity<>(data, headers, HttpStatus.OK);
-    }
-}
+//package org.learningproject.videohosting.controller;
+//
+//import org.learningproject.videohosting.model.Video;
+//import org.learningproject.videohosting.service.VideoService;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//@RestController
+//public class DownloadVideoController {
+//    private final VideoService videoService;
+//
+//    public DownloadVideoController(VideoService videoService) {
+//        this.videoService = videoService;
+//    }
+//
+//    @GetMapping("/video/watch/{id}")
+//    public ResponseEntity<byte[]> watchVideoById(@PathVariable Integer id){
+//        Video video = videoService.getVideo(id);
+//        if(video == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.parseMediaType(video.getContentType()));
+//        headers.set("Content-Disposition", "inline; mfilename=\"" + video.getFileName() + "\"");
+//        byte[] data = video.getData();
+//
+//        return new ResponseEntity<>(data, headers, HttpStatus.OK);
+//    }
+//}
--- src/main/java/org/learningproject/videohosting/controller/VideoRawInformationController.java
+++ src/main/java/org/learningproject/videohosting/controller/VideoRawInformationController.java
@@ -1,12 +1,8 @@
 package org.learningproject.videohosting.controller;
 
 import org.learningproject.videohosting.model.Video;
-import org.learningproject.videohosting.repository.VideoRepository;
 import org.learningproject.videohosting.service.VideoService;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,28 +10,23 @@ import org.springframework.web.server.ResponseStatusException;
 import java.io.IOException;
 
 @RestController
-public class VideoController {
+public class VideoRawInformationController {
 
     private final VideoService videoService;
 
-    public VideoController(VideoService videoService) {
+    public VideoRawInformationController(VideoService videoService) {
         this.videoService = videoService;
     }
 
     @GetMapping("/videos")
-    public Iterable<Video> getVideoList(){
+    public Iterable<Video> getVideoList() {
         return videoService.getAllVideos();
     }
 
-    @GetMapping("/video/{id}")
+    @GetMapping("/video/info/{id}")
     public Video getVideoById(@PathVariable Integer id){
         Video video = videoService.getVideo(id);
         if(video == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return video;
-    }
-
-    @PostMapping("/videos")
-    public Video createVideo(@RequestPart("data") MultipartFile videoFile) throws IOException {
-        return videoService.saveVideo(videoFile.getOriginalFilename(), videoFile.getContentType(), videoFile.getBytes());
     }
 }
